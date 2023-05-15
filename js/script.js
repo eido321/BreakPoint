b7dd975921d5211259f1532c4258251343d7487c
eido peretz <eido2488@gmail.com>
2023-05-15 13:27:41 +0300

    formPage v3

--- js/script.js
+++ js/script.js
@@ -32,55 +32,49 @@ hamburgerButton.click(function () {
     })
 })()
 
-let goBack = function () {
-    $("#cancel").on("click", function () {
-        history.back();
+const cancelButton = document.getElementById('cancel');
+
+if (cancelButton) {
+    cancelButton.addEventListener('click', function (event) {
+        event.preventDefault();
     });
+}
+let goBack = function () {
+    history.back();
 };
 
 
-const inputElement = document.getElementById("inputGroupFile01");
-const fileCountElement = document.getElementById("fileCount");
-inputElement.addEventListener("change", handleFiles, false);
 
-let files = [];
+if (document.getElementById("formFunc")) {
 
-function handleFiles() {
-    const fileList = this.files;
+    const inputElement = document.getElementById("inputGroupFile01");
+    const fileCountElement = document.getElementById("fileCount");
+    inputElement.addEventListener("change", handleFiles, false);
+    let files = [];
+    function handleFiles() {
+        const fileList = this.files;
 
-    for (let i = 0; i < fileList.length; i++) {
-        files.push(fileList[i]);
+        for (let i = 0; i < fileList.length; i++) {
+            files.push(fileList[i]);
+        }
+        displayFileList();
     }
-
-    // do something with the files array, like display a list of file names
-    displayFileList();
-}
-
-function displayFileList() {
-    const fileListContainer = document.getElementById("fileListContainer");
-    fileListContainer.innerHTML = "";
-
-    for (let i = 0; i < files.length; i++) {
-        const fileName = files[i].name;
-        const listItem = document.createElement("li");
-        listItem.textContent = fileName;
-
-        // create a remove icon to remove the file from the list
-        const removeIcon = document.createElement("div");
-        removeIcon.classList.add("remove-icon");
-        removeIcon.addEventListener("click", function () {
-            // remove the file from the array and re-render the list
-            files.splice(i, 1);
-            displayFileList();
-        });
-
-        // add the remove icon to the list item
-        listItem.appendChild(removeIcon);
-
-        // add the list item to the file list container
-        fileListContainer.appendChild(listItem);
+    function displayFileList() {
+        const fileListContainer = document.getElementById("fileListContainer");
+        fileListContainer.innerHTML = "";
+        for (let i = 0; i < files.length; i++) {
+            const fileName = files[i].name;
+            const listItem = document.createElement("li");
+            listItem.textContent = fileName;
+            const removeIcon = document.createElement("div");
+            removeIcon.classList.add("remove-icon");
+            removeIcon.addEventListener("click", function () {
+                files.splice(i, 1);
+                displayFileList();
+            });
+            listItem.appendChild(removeIcon);
+            fileListContainer.appendChild(listItem);
+        }
+        fileCountElement.textContent = `${files.length} file${files.length !== 1 ? 's' : ''}`;
     }
-
-    // update the file count in the input field
-    fileCountElement.textContent = `${files.length} file${files.length !== 1 ? 's' : ''}`;
 }
